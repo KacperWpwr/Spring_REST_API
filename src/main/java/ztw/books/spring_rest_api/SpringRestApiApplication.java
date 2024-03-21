fdd97d6a5d147783f6a707f6bb4a2487951b457b
Kacper Wieczorek <kacper.a.wieczorek1@gmail.com>
2024-03-21 21:12:40 +0100

    add openapi 3.0 instead of swagger2

--- src/main/java/ztw/books/spring_rest_api/SpringRestApiApplication.java
+++ src/main/java/ztw/books/spring_rest_api/SpringRestApiApplication.java
@@ -3,11 +3,11 @@ package ztw.books.spring_rest_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 //@EnableSwagger2
+
 public class SpringRestApiApplication {
 
     public static void main(String[] args) {
